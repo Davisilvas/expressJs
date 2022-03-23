7da0fa26a1cb86ef4316c3b5d449a2416b97e0df
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-23 09:58:49 -0300

    learned how to upload static arquives with express

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')))
+// app.use("/meusitte", express.static(path.join(__dirname, 'client')))
+
 
 const port = 5000;
 app.listen(port, () =>{
