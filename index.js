3973e36fb038cf37e3240265b99aaca3cfc90b15
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-24 10:31:40 -0300

    learned get in express

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client')))
 // app.use("/meusitte", express.static(path.join(__dirname, 'client')))
 
+app.get("/", (req, res) =>{
+    // res.set('content-type', 'text/plain')
+    res.type(".html");
+    res.type('.html') => 'text/html'
+
+    // res.type('html') => 'text/html'
+    // res.type('txt') => 'text/plain'
+    // res.type('json') => 'application/json'
+    // res.type('application/json') => 'application/json'
+    // res.type('png') => 'image/png'
+    
+    res.send("<h1>hello world from GET</h1>");
+})
 
 const port = 5000;
 app.listen(port, () =>{
