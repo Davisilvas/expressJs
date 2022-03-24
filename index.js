594d5c4f316101956fbedc89594dd55a87f28977
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-24 11:10:14 -0300

    get put post delete

--- index.js
+++ index.js
@@ -4,21 +4,23 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client')))
-// app.use("/meusitte", express.static(path.join(__dirname, 'client')))
+// app.use("/meusite", express.static(path.join(__dirname, 'client')))
 
 app.get("/", (req, res) =>{
-    // res.set('content-type', 'text/plain')
     res.type(".html");
-
-    // res.type('.html') => 'text/html'
-    // res.type('html') => 'text/html'
-    // res.type('txt') => 'text/plain'
-    // res.type('json') => 'application/json'
-    // res.type('application/json') => 'application/json'
-    // res.type('png') => 'image/png'
-
-
     res.send("<h1>hello world from GET</h1>");
+})
+
+app.put("/", (req, res) =>{
+    res.send("<h1>hello world from PUT</h1>");
+})
+
+app.post("/", (req, res) =>{
+    res.send("<h1>hello world from POST</h1>");
+})
+
+app.delete("/", (req, res) =>{
+    res.send("<h1>hello world from DELETE</h1>");
 })
 
 const port = 5000;
