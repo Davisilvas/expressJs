aa66173636cf4cc18c290a2c699fe1245148c11e
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-24 20:09:43 -0300

    ......

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'client')))
-// app.use("/meusite", express.static(path.join(__dirname, 'client')))
-
 app.get("/", (req, res) =>{
-    res.send("<h1>hello world from GET</h1>");
-
+    console.log(req);
+    res.send("<h1> Hello World </h1>")
 })
 
-app.put("/", (req, res) =>{
-    res.send("<h1>hello world from PUT</h1>");
-})
-
-app.post("/", (req, res) =>{
-    res.send("<h1>hello world from POST</h1>");
-})
-
-app.delete("/", (req, res) =>{
-    res.send("<h1>hello world from DELETE</h1>");
-})
-
-const port = 5000;
-app.listen(port, () =>{
-    console.log(`Server Running on Port:${port}`);
-})
+app.listen(3000, () => console.log("server rodando..."));
