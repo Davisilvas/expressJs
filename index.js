fe7f3c0ed974c18bbe34e28a418ef458469b7076
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-30 17:24:39 -0300

    Learned how to use middlewares

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser'); 
 
-app.get("/", (req, res) =>{
-    console.log(req);
-    res.send("<h1> Hello World </h1>")
-})
+// let consolemethod =  (req, res, next) =>{
+//     console.log(req.method); 
+//     next();
+//     // next("Erro");
+// }
+
+let consoleBody =  (req, res, next) =>{
+    console.log(req.body); 
+    next();
+    // next("Erro");
+}
+
+let hello =  (req, res) =>{
+    // console.log(req);
+    res.send("<h1> pega o server do pai </h1>")
+}
+
+app.use ("/", bodyParser.json());
+app.use ("/", consoleBody)
+
+app.get("/", hello)
+app.post("/", hello)
 
 app.listen(3000, () => console.log("server rodando..."));
